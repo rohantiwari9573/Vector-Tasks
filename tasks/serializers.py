39c0752336a9872164c172bb78b9681da94dfbe4
Rohan <rohantiwari166@gmail.com>
2026-06-14 00:33:56 +0530

    Fix RegisterSerializer import issue

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Task
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"]
+        )
+        return user
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -7,10 +24,3 @@ class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = [
-            "id",
-            "title",
-            "completed",
-            "created_at",
-            "username",
-        ]
