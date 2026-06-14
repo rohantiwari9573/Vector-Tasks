a172b3c934ef215646bc5b59bb4fece69832b610
Rohan <rohantiwari166@gmail.com>
2026-06-14 14:25:29 +0530

    Switch deployment database to PostgreSQL

--- task-manager-frontend/src/services/api.js
+++ task-manager-frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-1-ndia.onrender.com/api",
+  baseURL: "https://task-manager-1-ndia.onrender.com/api/",
+  timeout: 15000,
 });
 
 API.interceptors.request.use((req) => {
