130cedacb5f1c6420f6dc4835efb5e306e473cda
Rohan <rohantiwari166@gmail.com>
2026-08-25 14:04:15 +0530

    Enforce password validators on registration
    RegisterSerializer called create_user() directly, bypassing the
    four AUTH_PASSWORD_VALIDATORS configured in settings.py entirely.
    Confirmed passwords like "1234" and a password identical to the
    username were both accepted before this fix.

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from .models import Task
 
 
@@ -10,6 +11,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "password": {"write_only": True}
         }
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
